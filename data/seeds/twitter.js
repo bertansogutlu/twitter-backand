3fc1f5f2c778061ddbfc688e08abe8278d9867f0
Bertan Sogutlu <bertansogutlu@gmail.com>
2023-04-15 02:43:18 +0300

    seed datasi olusturuldu

--- data/seeds/twitter.js
+++ data/seeds/twitter.js
@@ -4,40 +4,55 @@
  */
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
-  await knex('order_toppings').truncate()
-  await knex('orders').truncate()
-  await knex('toppings').truncate()
-  await knex('pizzas').truncate()
+  await knex('fallowers').truncate()
+  await knex('followings').truncate()
+  await knex('comments').truncate()
+  await knex('likes').truncate()
+  await knex('favorites').truncate()
+  await knex('retweets').truncate()
+  await knex('tweets').truncate()
   await knex('users').truncate()
-  await knex('roles').truncate()
 
-  await knex('roles').insert([
-    {role_id: 1, role: 'admin'},
-    {role_id: 2, role: 'user'},
-  ]);
+
   await knex('users').insert([
-    {username: "Defne", surname: 'Atik', email: "defne@gmail.com", password: '$2a$10$dFwWjD8hi8K2I9/Y65MWi.WU0qn9eAVaiBoRSShTvuJVGw8XpsCiq', role_id: 2},
-    {username: "Bertan", surname: 'Sogutlu', email: "bertan@gmail.com", password: '$2a$10$dFwWjD8hi8K2I9/Y65MWi.WU0qn9eAVaiBoRSShTvuJVGw8XpsCiq', role_id: 1},
+    {email: "bertan@gmail.com", username: "bertan", password: '$2a$10$dFwWjD8hi8K2I9/Y65MWi.WU0qn9eAVaiBoRSShTvuJVGw8XpsCiq', role: "admin"},
+    {email: "defne@gmail.com", username: "defne", password: '$2a$10$dFwWjD8hi8K2I9/Y65MWi.WU0qn9eAVaiBoRSShTvuJVGw8XpsCiq', role: "user"},
+    {email: "pasa@gmail.com", username: "pasa", password: '$2a$10$dFwWjD8hi8K2I9/Y65MWi.WU0qn9eAVaiBoRSShTvuJVGw8XpsCiq', role: "user"},
   ]);
-  await knex('pizzas').insert([
-    {pizza: 'Pepperoni', description: 'Hot', price: '150'},
-    {pizza: 'Fungi', description: 'Soft', price: '120'},
+  await knex('tweets').insert([
+    {user_id: 1, tweet: 'Tweet one'},
+    {user_id: 2, tweet: 'Tweet two'},
+    {user_id: 3, tweet: 'Tweet three'},
   ]);
-  await knex('toppings').insert([
-    {topping: 'Pepperoni'},
-    {topping: 'Sausage'},
-    {topping: 'Mushrooms'},
-    {topping: 'Chicken'},
-    {topping: 'Onions'},
-    {topping: 'Peppers'},
+  await knex('retweets').insert([
+    {user_id: 1, tweet_id: 3},
+    {user_id: 2, tweet_id: 2},
+    {user_id: 3, tweet_id: 1},
   ]);
-  await knex('orders').insert([
-    {user_id: 1, dough: 'thick', size: 'small', quantity: 1, note: 'Extra hot', status: 'Preparing', price: '150', pizza_id: 1},
-    {user_id: 2, dough: 'thick', size: 'small', quantity: 1, note: 'Extra sauce', status: 'Preparing', price: '120', pizza_id: 2},
+  await knex('favorites').insert([
+    {user_id: 1, tweet_id: 3},
+    {user_id: 2, tweet_id: 2},
+    {user_id: 3, tweet_id: 1},
   ]);
-  await knex('order_toppings').insert([
-    {order_id: 1, topping_id: 1},
-    {order_id: 2, topping_id: 2},
+  await knex('likes').insert([
+    {user_id: 1, tweet_id: 3},
+    {user_id: 2, tweet_id: 2},
+    {user_id: 3, tweet_id: 1},
+  ]);
+  await knex('comments').insert([
+    {user_id: 1, comment: "Comment one"},
+    {user_id: 2, comment: "Comment two"},
+    {user_id: 3, comment: "Comment three"},
+  ]);
+  await knex('fallowings').insert([
+    {user_id: 1, following_user_id: 3},
+    {user_id: 2, following_user_id: 2},
+    {user_id: 3, following_user_id: 1},
+  ]);
+  await knex('fallowers').insert([
+    {user_id: 1, fallower_user_id: 3},
+    {user_id: 2, fallower_user_id: 2},
+    {user_id: 3, fallower_user_id: 1},
   ]);
 
 };

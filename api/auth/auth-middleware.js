16965b6163a5f16e22267fba98cfcc5f761fc19e
Bertan Sogutlu <bertansogutlu@gmail.com>
2023-04-15 05:17:40 +0300

    register role atama kontrolu yapildi

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,20 +1,29 @@
 const usersModel = require("../users/users-model");
+const { ADMIN_PASSWORD } = require("../../config");
 
 const validateRegisterPayload = async (req, res, next) => {
   try {
     if (!req.body.username) {
-      res.status(400).json({ message: "Username is required" });
+      res.status(400).json({ message: "username is required" });
     } else if (!req.body.email) {
-      res.status(400).json({ message: "Email is required" });
+      res.status(400).json({ message: "email is required" });
     } else if (!req.body.password) {
-      res.status(400).json({ message: "Password is required" });
+      res.status(400).json({ message: "password is required" });
     } else {
-        const emailIsExist = await usersModel.getByEmail(req.body.email)
-        if(emailIsExist){
-            res.status(400).json({emessage: `${req.body.email} has been already taken` });
-        }else(
-            next()
-        )
+        const emailIsExist = await usersModel.getByEmail(req.body.email);
+        if (emailIsExist) {
+            res
+            .status(400)
+            .json({ emessage: `${req.body.email} has been already taken` });
+        } else if (req.body.role) {
+            if (req.body.admin_password === ADMIN_PASSWORD) {
+            delete req.body.admin_password;
+            next();
+            console.log(ADMIN_PASSWORD)
+            } else {
+            res.status(403).json({ emessage: "admin_password is required" });
+            }
+      } else next();
     }
   } catch (error) {
     next(error);
@@ -24,9 +33,9 @@ const validateRegisterPayload = async (req, res, next) => {
 const validateLoginPayload = async (req, res, next) => {
   try {
     if (!req.body.email) {
-      res.status(400).json({ message: "Email is required" });
+      res.status(400).json({ message: "email is required" });
     } else if (!req.body.password) {
-      res.status(400).json({ message: "Password is required" });
+      res.status(400).json({ message: "password is required" });
     } else {
       next();
     }
--- config.js
+++ config.js
@@ -3,11 +3,13 @@ require('dotenv').config()
 PORT = process.env.PORT || 4000
 NODE_ENV = process.env.NODE_ENV || "development"
 JWT_SECRET = process.env.SECRET || "hede"
+ADMIN_PASSWORD = process.env.PASSWORD || 1234
 
 
 
 module.exports = {
     PORT,
     NODE_ENV,
-    JWT_SECRET
+    JWT_SECRET,
+    ADMIN_PASSWORD
 }
